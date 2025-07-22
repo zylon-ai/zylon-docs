bd1e7cefc0912e920583491efaf1cdc6e3e56771
Pablo Orgaz <pabloogc@gmail.com>
2025-07-22 11:09:15 +0200

    initial commit

---
+++ update-spec.sh
@@ -0,0 +1,45 @@
+cd $(dirname $0) || exit 1
+cd api-reference || exit 1
+
+curl -q https://zylon.me/api/openapi.json -o workspace.json || exit 1
+curl -q https://zylon.me/gpt/openapi.json -o pgpt.json || exit 1
+
+function jq-i() {
+    # jq in place
+    jq "$1" $2 > $2.tmp && mv $2.tmp $2
+    rm -f $2.tmp
+}
+
+function sed-i() {
+    # sed in place, required for macOS, works with -i on linux
+    sed "$1" $2 > $2.tmp && mv $2.tmp $2
+    rm -f $2.tmp
+}
+
+query='.paths
+         | to_entries
+         | map({path: .key, methods: (.value | to_entries
+             | map(select(.value.summary | not)
+             | {method: .key, operation: .value}))})
+         | map(select(.methods != []))'
+
+# Validate all endpoints have a summary
+out="$(jq -r $query workspace.json)"
+if [ "$out" != "[]" ]; then
+    echo "Workspace endpoints missing summaries"
+    echo "$out"
+    exit 1
+fi
+
+out="$(jq -r $query pgpt.json)"
+if [ "$out" != "[]" ]; then
+    echo "PGPT endpoints missing summaries"
+    echo "$out"
+    exit 1
+fi
+
+mkdir -p workspace/
+npx @mintlify/scraping@latest openapi-file workspace.json -o workspace/
+
+mkdir -p pgpt
+npx @mintlify/scraping@latest openapi-file pgpt.json -o pgpt/

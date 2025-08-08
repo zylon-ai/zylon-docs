95790fb9647480ce994af214e367b0a63c5155f0
Pablo Orgaz <pabloogc@gmail.com>
2025-08-08 12:06:23 +0200

    fix: broken CI secret

--- update-spec.sh
+++ update-spec.sh
@@ -2,12 +2,15 @@ cd $(dirname $0) || exit 1
 cd api-reference || exit 1
 
 host=$1
+token=$2
 if [ -z "$host" ]; then
-  host="https://zylon.me"
+  host="https://demo.zylon.ai"
 fi
 
-curl -s $host/api/openapi.json -o workspace.json || exit 1
-curl -s $host/gpt/openapi.json -o pgpt.json || exit 1
+curl -s $host/api/openapi.json -o workspace.json \
+-H "Authorization: Bearer $token" || exit 1
+curl -s $host/api/gpt/openapi.json -o pgpt.json \
+-H "Authorization: Bearer $token" || exit 1
 
 mint openapi-check workspace.json || exit 1
 mint openapi-check pgpt.json || exit 1
